53e2353b0be7dace336c77ca337bafc62b7e18e4
AMAN TANWAR <amantanwar1996@gmail.com>
2018-07-23 21:23:28 +0530

    Add files via upload

---
+++ take_a_break_original.py
@@ -0,0 +1,11 @@
+import time
+import webbrowser
+
+break_count=0
+till=3
+ 
+
+while till>break_count :
+      time.sleep(5)
+      webbrowser.open("D:\series\sherlock holmes\webpage\webpage\discipline.htm")
+      break_count=break_count+1
